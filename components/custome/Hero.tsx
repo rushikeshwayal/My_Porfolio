31d186decb63a7f7b1235033c3038ec3ebca19b2
rushikeshwayal <rushikeshwayal6@gmail.com>
2025-01-08 17:27:15 +0530

    this is first commit

---
+++ components/custome/Hero.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import WordRotate from "../ui/word-rotate";
+import  { RainbowButton }  from '../ui/rainbow-button';
+import { cn } from "@/lib/utils";
+import GridPattern from "@/components/ui/grid-pattern";
+import NavBar from './NavBar';
+import ShinyButton from "@/components/ui/shiny-button";
+export default function Hero() {
+  return (
+
+
+
+
+
+<div >
+    <div className='flex justify-center'>
+<NavBar />
+</div>
+<div className="hero-container flex flex-col items-center justify-center h-screen text-center">
+      <div className="w-full flex flex-wrap justify-center mb-10 gap-10 items-center">
+        <h1 className="text-5xl sm:text-6xl md:text-7xl font-extrabold  tracking-tight">
+          Hi there, I am
+        </h1>
+        <div className=" w-[400px] " >
+          <WordRotate
+            className="text-5xl sm:text-6xl md:text-7xl font-extrabold text-green-400"
+            words={["Rushikesh", "@Mantrix"]}
+          />
+        </div>
+      </div>
+      <div>
+        <p className="text-lg sm:text-xl md:text-2xl max-w-2xl">
+          AI Developer & Full Stack Developer
+        </p>
+      </div>
+      <div className='mt-10 space-x-5'>
+      {/* <ShinyButton>Shiny Button</ShinyButton> */}
+      
+      <RainbowButton>Lets Talk</RainbowButton>
+       <RainbowButton>Download Resume</RainbowButton>
+       </div>
+    </div>
+<GridPattern
+
+  width={30}
+  height={30}
+  x={-1}
+  y={-1}
+  strokeDasharray={"4 2"}
+  className={cn(
+    "[mask-image:radial-gradient(1000px_circle_at_center,white,transparent)] -z-10",
+  )}
+/>
+</div>
+  );
+}
---
+++ components/custome/NavBar.tsx
@@ -0,0 +1,70 @@
+
+
+import React from 'react';
+import Link from 'next/link';
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+
+export default function NavBar() {
+  return (
+    <div className='border-[2px]   border-white px-8 py-2 rounded-3xl top-5 fixed'>
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+
+            <BreadcrumbLink>
+              <Link href="/">About</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+
+          <BreadcrumbSeparator />
+
+          <BreadcrumbItem>
+            <BreadcrumbLink>
+              <Link href="/components">Services</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+
+          <BreadcrumbSeparator />
+
+ <BreadcrumbItem>
+            <BreadcrumbLink>
+              <Link href="/components">Experience</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+
+<BreadcrumbSeparator />
+
+<BreadcrumbItem>
+            <BreadcrumbLink>
+              <Link href="/components">Skills</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+
+<BreadcrumbSeparator />
+
+<BreadcrumbItem>
+            <BreadcrumbLink>
+              <Link href="/components">Project</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+
+<BreadcrumbSeparator />
+
+          <BreadcrumbItem>
+          <BreadcrumbLink>
+             <Link href="/components">Contact</Link>
+           </BreadcrumbLink>
+          </BreadcrumbItem>
+          
+        </BreadcrumbList>
+      </Breadcrumb>
+    </div>
+  );
+}
